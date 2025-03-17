8d8f88aa21e19218d6a4e8053b2b18df239c53eb
spanovs01 <spanovs01frag@egmail.com>
2025-03-17 21:41:01 +0300

    ADD working example of finding human faces by camera

--- camera_and_rotating.py
+++ camera_and_rotating.py
@@ -1,31 +1,36 @@
 import cv2
 import numpy as np
+import time
 from picamera2 import Picamera2
     
 def Face_Detector(picamera, display=True):                      # TODO: threshold на близость человека к голове
     face_detector = cv2.CascadeClassifier("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml")
     while True:
-        frame = picamera.capture_array("main")  
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = picamera.capture_array("lores")
+        image = cv2.cvtColor(frame, cv2.COLOR_YUV2BGR_I420)
         faces = face_detector.detectMultiScale(image, 1.1, 3) # TODO: неточная модельб видит лица там где их нет
-        print(faces)
-        (x, y, w, h) = faces[0]
-        print(x,y,w,h)
-        img_with_face = cv2.rectangle(image, (x, y), (x+h, y+h), (0, 255, 0), 5)
+        # print(faces)
+        if len(faces) > 0:
+            (x, y, w, h) = faces[0]
+            print(x,y,w,h)
+            img_with_face = cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 5)
+        else:
+            img_with_face = image
+
         if display:
             cv2.imshow("Detecting people", img_with_face)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        time.sleep(0.03)
     cv2.destroyAllWindows()
     
     
     
 if __name__ == "__main__":
     picam2 = Picamera2()
-    picam2.configure(picam2.create_preview_configuration(main={"format": 'BGR888', "size": (1600, 1300)})) # govniche - TODO rewrite loadcoeffs
+    picam2.configure(picam2.create_preview_configuration(lores={"size": (320, 240)}, encode="lores")) # govniche - TODO rewrite loadcoeffs
     picam2.set_controls({"FrameDurationLimits": (16700, 16700)})
     picam2.set_controls({"AwbEnable": True})
     picam2.start()
-    while True:
-        Face_Detector(picam2, True)
+    Face_Detector(picam2, True)
         
